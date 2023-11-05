3472b7a4c1ede16dc8aca3f4bc891c0686b1875c
Shiham123 <s36651786@gmail.com>
2023-11-05 10:51:23 +0600

    tabs section complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import BannerSection from '../Component/BannerSection';
+import TabsSection from '../Component/TabsSection';
 
 const HomePage = () => {
   return (
     <>
       <BannerSection />
+      <TabsSection />
     </>
   );
 };
