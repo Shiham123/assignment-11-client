3503f997f4a7711b454b0390ad3c570f9112c387
Shiham123 <s36651786@gmail.com>
2023-11-06 17:24:09 +0600

    with axios get successfully from server based on login user

--- src/pages/MyPostedJob.jsx
+++ src/pages/MyPostedJob.jsx
@@ -1,4 +1,19 @@
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../Context/context';
+import axios from 'axios';
+
 const MyPostedJob = () => {
+  const context = useContext(AppContext);
+  const { user } = context;
+  const contextEmail = user?.email;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/jobPosted?email=${contextEmail}`)
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  }, [contextEmail]);
+
   return <div>posted job</div>;
 };
 
